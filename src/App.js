83cfd92ed331a8a1cfe845ac0751a5fe00c8e7b0
PaLanzi <palanzillotta@googlemail.com>
2022-04-09 17:00:28 +0200

    added slug and singlerecipe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import Recipies from './components/Recipies/Recipies';
+import SingleRecipie from './components/SingleRecipie/SingleRecipie';
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
 const [loading, setLoading] = useState(false);
 const [recipies, setRecipies] = useState([]);
+const [singleRecipie, setSingleRecipie] = useState([]);
 const [images, setImages] = useState([]);
 
   useEffect(() => {
@@ -32,6 +35,36 @@ const [images, setImages] = useState([]);
     fetchRecipies();
   }, []);
   console.log(recipies)
+
+  const testfunc = () => {
+    console.log("test")
+  }
+
+  // useEffect(() => {
+  //   const fetchSingleRecipie = async () => {
+  //     const url = `https://cdn.contentful.com/spaces/xzt8xx1icvbr/environments/master/entries/${}?access_token=${process.env.REACT_APP_API_KEY}`;
+  //     try {
+  //       setLoading(true);
+  //       const response = await fetch(url);
+  
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         console.log(data)
+          
+  //         setSingleRecipie(data.items);
+          
+  //         setLoading(false);
+  //       } else {
+  //         console.error("Fetch error!");
+  //         alert("There has been an error!");
+  //       }
+  //     } catch (e) {
+  //       console.log(e.message);
+  //     }
+  //   };
+  //   // fetchSingleRecipie();
+  // }, []);
+  // console.log(recipies)
 
   useEffect(() => {
     const fetchRecipies = async () => {
@@ -65,8 +98,11 @@ const [images, setImages] = useState([]);
 
   
   return (
-    <div class="container">
-      <Recipies recipies={recipies} images={images}></Recipies>
+    <div className="container">
+      <Routes>
+      <Route path="/" element={<Recipies recipies={recipies} images={images} testfunc={testfunc}/>} />
+      <Route path="/:slug" element={<SingleRecipie recipies={recipies} images={images} />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Recipies/Recipies.js
+++ src/components/Recipies/Recipies.js
@@ -1,33 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Recipies ({recipies, images}) {
-    return (
-        
-        <ul class="list-group">{recipies.map((recipe) => (
-            <div key={recipe.sys.id}>
-            <li class="list-group-item">{recipe.fields.header}</li>
-            <li class="list-group-item">{recipe.fields.description}</li>
-            <div class="container">{recipe.fields.ingridientTable.content.map((ingridient) =>(
-                <div>
-                {ingridient.content.map((ingridient) => (
-                    <li class="list-group-item">
-                        {ingridient.value}
-                    </li>
-                ))}
-                </div>
-
-
-            ))}</div>
-            <li class="list-group-item">{images.map((image) => (
-                image.fields.title == recipe.fields.header &&
-                <img key={image.sys.id} src={image.fields.file.url}/>
-            ))}</li>
-            </div>
-        ))}
-        </ul>
-        
-        
-    )
+function Recipies({ recipies, images, testfunc }) {
+  return (
+    <ul className="list-group">
+      {recipies.map((recipe) => (
+        <div key={recipe.sys.id}>
+          <Link to={recipe.fields.slug} onClick={testfunc}>
+            <li className="list-group-item">
+              <h1>{recipe.fields.header}</h1>
+            </li>
+            <li className="list-group-item">
+              {images.map(
+                (image) =>
+                  image.fields.title == recipe.fields.header && (
+                    <img key={image.sys.id} src={image.fields.file.url} />
+                  )
+              )}
+            </li>
+          </Link>
+        </div>
+      ))}
+    </ul>
+  );
 }
 
 export default Recipies;
